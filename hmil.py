0863fab19976561444bf890b65c5428248c4a56c
nixoletas <nicholetas@gmail.com>
2023-05-26 18:31:28 -0400

    deleted some files and changed some names

--- hmil.py
+++ hmil.py
@@ -3,19 +3,23 @@ from selenium.webdriver.common.by import By
 
 import time
 
-driver = webdriver.Chrome()
+
+op = webdriver.ChromeOptions()
+op.add_argument('headless')
+driver = webdriver.Chrome(options=op)
+
 driver.get('https://sigh.hmilacg.eb.mil.br/paciente')
 driver.implicitly_wait(0.5)
 
 username_button = driver.find_element(By.CSS_SELECTOR, "input")
+print(username_button)
 username_button.send_keys("150129")
 
 password_button = driver.find_element(By.CSS_SELECTOR, "input[placeholder='Senha']")
 password_button.send_keys('92349')
+print(password_button)
 
 login_button = driver.find_element(By.NAME, "btn-login")
 login_button.click()
-
-print(password_button)
 
 time.sleep(1000)
