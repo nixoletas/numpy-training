4431cbbd5375759294d274635420d706918bd485
nixoletas <nicholetas@gmail.com>
2023-05-26 20:54:17 -0400

    training

---
+++ db-pandas-work.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+dataset = pd.read_csv('db.csv', sep = ';')
+
+def km_media(dataset, ano_atual):
+    for item in dataset.items():
+        result = item[1]['km'] / (ano_atual - item[1]['ano'])
+        print(result)
---
+++ np.py
@@ -0,0 +1,4 @@
+def sayHello(name, phrase):
+  print(f'Hi {name}! {phrase}')
+  
+sayHello('nick','How are you?')
